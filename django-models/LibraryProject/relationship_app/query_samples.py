10fd2e9ae5ac1ef9b02e03736bb8298016ce1bee
Joshua Akuma <joshuakuma.ja@gmail.com>
2026-06-02 16:46:20 +0100

    query_samples.py modified

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,13 +1,14 @@
 from models import *
 
-
-author = Author.objects.get(name = "Joshua")
+author_name = "Joshua"
+author = Author.objects.get(name = author_name)
 books_by_author = Book.objects.filter(author = author)
 
 
 for book in books_by_author:
     print(book.title)
 
-library = Library.objects.get(name = "Mosque")
+library_name = "mosque"
+library = Library.objects.get(name = library_name)
 books_in_lib = library.books.all()
 librarian = Librarian.objects.filter(library = library)
