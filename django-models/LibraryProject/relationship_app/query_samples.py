fa71a278dccb34542e5d1901b721be19b70b21e3
Joshua Akuma <joshuakuma.ja@gmail.com>
2026-06-02 16:39:02 +0100

    added a query

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,6 @@
 from models import *
 
+books
 author = Author.objects.get(name = "Joshua")
 books_by_author = Book.objects.filter(author = author)
 
@@ -8,4 +9,5 @@ for book in books_by_author:
     print(book.title)
 
 library = Library.objects.get(name = "Mosque")
+books_in_lib = library.all()
 librarian = Librarian.objects.filter(library = library)
