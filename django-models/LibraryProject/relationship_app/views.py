6079e94e97cc226129eb60e69502acb2873ca0bf
Joshua Akuma <joshuakuma.ja@gmail.com>
2026-06-04 14:23:43 +0100

    created views for book list and details

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Book
 from .models import Library
 from django.views import View
-from django.views.generic import DetailView
+from django.views.generic.detail import DetailView
 
 # Create your views here.
 
