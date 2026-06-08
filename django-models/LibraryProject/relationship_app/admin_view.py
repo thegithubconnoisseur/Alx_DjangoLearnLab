e9a11512d8d1294a06bee060563d50fc61f8c85a
Joshua Akuma <joshuakuma.ja@gmail.com>
2026-06-08 18:34:12 +0100

    user role permissions

--- django-models/LibraryProject/relationship_app/admin_view.py
+++ django-models/LibraryProject/relationship_app/admin_view.py
@@ -2,9 +2,11 @@ from django.shortcuts import render
 from django.contrib.auth.decorators import user_passes_test
 from .role_checks import is_admin
 
-'''
+def is_admin(user):
+    return (user.is_authenticated and hasattr(user, "userprofile") and user.userprofile.role == "ADMIN")
+
 @user_passes_test(is_admin)
 def admin_view(request):
     return render(request, "relationship_app/admin_view.html")
-'''
+
 
