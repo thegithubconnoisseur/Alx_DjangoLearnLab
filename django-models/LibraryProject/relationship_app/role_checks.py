fba6ccb6dfa47f2ca082d610e81137de92fe8548
Joshua Akuma <joshuakuma.ja@gmail.com>
2026-06-08 18:14:23 +0100

    Implemented user role permissions

--- django-models/LibraryProject/relationship_app/role_checks.py
+++ django-models/LibraryProject/relationship_app/role_checks.py
@@ -1,8 +1,8 @@
 def is_admin(user):
-    return user.is_authenticated and user.profile.role == "ADMIN"
+    return user.is_authenticated and user.userprofile.role == "ADMIN"
 
 def is_librarian(user):
-    return user.is_authenticated and user.profile.role == "LIBRARIAN"
+    return user.is_authenticated and user.userprofile.role == "LIBRARIAN"
 
 def is_member(user):
-    return user.is_authenticated and user.profile.role == "MEMBER"
+    return user.is_authenticated and user.userprofile.role == "MEMBER"
